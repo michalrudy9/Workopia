5253a970b9ebd6be44d57b0c00334cde04059170
Michał Rudy <michallrudy9@gmail.com>
2024-06-10 15:06:32 +0200

    Added next helper functions

--- helpers.php
+++ helpers.php
@@ -4,3 +4,25 @@ function basePath(string $path = ""): string
 {
     return __DIR__ . "/" . $path;
 }
+
+function loadView(string $name): void
+{
+    $viewPath = basePath("views/{$name}.php");
+
+    if (file_exists($viewPath)) {
+        require_once $viewPath;
+    } else {
+        echo "View '{$name} not found!'";
+    }
+}
+
+function loadPartial(string $name): void
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require_once $partialPath;
+    } else {
+        echo "Partial '{$name} not found!'";
+    }
+}
