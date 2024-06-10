aa7583b1cb171ab864d6e46f5117eb086e2fd851
Michał Rudy <michallrudy9@gmail.com>
2024-06-10 17:11:04 +0200

    Added routing to appliaction

--- public/index.php
+++ public/index.php
@@ -2,4 +2,17 @@
 
 require_once "../helpers.php";
 
-loadView("home");
+$routes = [
+    "/" => "controllers/home.php",
+    "/listings" => "controllers/listings/index.php",
+    "/listings/create" => "controllers/listings/create.php",
+    "404" => "controllers/error/404.php",
+];
+
+$uri = $_SERVER["REQUEST_URI"];
+
+if (array_key_exists($uri, $routes)) {
+    require_once basePath($routes[$uri]);
+} else {
+    require_once basePath($routes["404"]);
+}
