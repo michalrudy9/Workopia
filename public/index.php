ad96de138416cf6967caaeefa7413553cb21668d
Michał Rudy <michallrudy9@gmail.com>
2024-06-11 20:55:02 +0200

    Created database class and added to app

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 require_once "../helpers.php";
 require_once basePath("Router.php");
+require_once basePath("Database.php");
+
+$config = require_once basePath("config/db.php");
+
+$db = new Database($config);
 
 $uri = $_SERVER["REQUEST_URI"];
 $method = $_SERVER["REQUEST_METHOD"];
