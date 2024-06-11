9773a432bc3db4fdd212e564b7eb4b4eea53a4bc
Michał Rudy <michallrudy9@gmail.com>
2024-06-11 22:27:18 +0200

    Added format salary function

--- helpers.php
+++ helpers.php
@@ -5,11 +5,12 @@ function basePath(string $path = ""): string
     return __DIR__ . "/" . $path;
 }
 
-function loadView(string $name): void
+function loadView(string $name, array $data = []): void
 {
     $viewPath = basePath("views/{$name}.php");
-    
+
     if (file_exists($viewPath)) {
+        extract($data);
         require_once $viewPath;
     } else {
         echo "View '{$name} not found!'";
@@ -39,4 +40,9 @@ function inspectAndDie(mixed $value): void
     echo "<pre>";
     die(var_dump($value));
     echo "</pre>";
+}
+
+function formatSalary(string $salary): string
+{
+    return '$' . number_format(floatval($salary));
 }
