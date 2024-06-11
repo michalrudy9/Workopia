c6ca3095a44404cd57b99ad3af56de750d166ec7
Michał Rudy <michallrudy9@gmail.com>
2024-06-11 21:40:37 +0200

    Added some fixes and query method

--- Database.php
+++ Database.php
@@ -4,24 +4,37 @@ class Database
 {
     public $conn;
 
-    public function __construct(array $config): void
+    public function __construct(array $config)
     {
         $dsn = "mysql:host{$config["host"]};port={$config["port"]};dbname={$config["dbname"]}";
 
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ];
 
         try {
             $this->conn = new PDO(
                 $dsn,
                 $config["username"],
-                $config["password"]
+                $config["password"],
+                $options
             );
         } catch (PDOException $e) {
             throw new Exception(
                 "Database connection failed: {$e->getMessage()}"
             );
+        }
+    }
+
+    public function query(string $query): PDOStatement
+    {
+        try {
+            $sth = $this->conn->prepare($query);
+            $sth->execute();
+            return $sth;
+        } catch (PDOException $e) {
+            throw new Exception("Query failed to execute: {$e->getMessage()}");
         }
     }
 }
