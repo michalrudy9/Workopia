7c56cc2613e1ac8cc6c1dad1a210021391f83c55
Michał Rudy <michallrudy9@gmail.com>
2024-06-12 14:49:44 +0200

    Added show controller

---
+++ controllers/listings/show.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = require_once basePath("config/db.php");
+$db = new Database($config);
+
+$id = $_GET["id"] ?? "";
+
+$params = [
+    "id" => $id,
+];
+
+$listing = $db
+    ->query("select * from listings where id = :id", $params)
+    ->fetch();
+
+loadView("listings/show", ["listing" => $listing]);
