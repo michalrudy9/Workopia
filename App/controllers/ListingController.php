c317215a45b0dcdb268419d078c31061cd88b688
Michał Rudy <michallrudy9@gmail.com>
2024-06-13 13:50:53 +0200

    Added form validation for listing create

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use Framework\Database;
+use Framework\Validation;
 
 class ListingController
 {
@@ -44,5 +45,51 @@ class ListingController
         }
 
         loadView("listings/show", ["listing" => $listing]);
+    }
+
+    public function store(): void
+    {
+        $allowedFields = [
+            "title",
+            "description",
+            "salary",
+            "tags",
+            "company",
+            "address",
+            "city",
+            "state",
+            "phone",
+            "email",
+            "requirements",
+            "benefits",
+        ];
+
+        $newListingData = array_intersect_key(
+            $_POST,
+            array_flip($allowedFields)
+        );
+
+        $newListingData["user_id"] = 1;
+        $newListingData = array_map("sanitize", $newListingData);
+        $requiredFields = ["title", "description", "email", "city", "state"];
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (
+                empty($newListingData[$field]) ||
+                !Validation::string($newListingData[$field])
+            ) {
+                $errors[$field] = ucfirst($field) . " is required";
+            }
+        }
+
+        if (!empty($errors)) {
+            loadView("listings/create", [
+                "errors" => $errors,
+                "listing" => $newListingData,
+            ]);
+        } else {
+            echo "Success";
+        }
     }
 }
