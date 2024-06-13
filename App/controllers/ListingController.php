f43beb62d737ba508b96ea90ae1f33f2dbf88548
Michał Rudy <michallrudy9@gmail.com>
2024-06-13 16:19:21 +0200

    Added flash messages

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -137,6 +137,8 @@ class ListingController
 
         $this->db->query("DELETE FROM listings WHERE id = :id", $params);
 
+        $_SESSION["success_message"] = "Listing deleted successfully";
+
         redirect("/listings");
     }
 }
